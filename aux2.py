fea5e4c4cb22ee6cd350a567814c6d68ec56bafc
Daniel Glatthardt <daniel.ferreira.glatthardt@ufv.br>
2025-08-27 13:07:27 -0300

    arquivos desnecessários removidos

--- aux2.py
+++
@@ -1,9 +0,0 @@
-import config as cfg
-
-cfg.stored_info.email_content['feedback'] =  'a'
-
-def get_user_data():
-    return cfg.stored_info.email_content
-get_user_data.__doc__ = cfg.ToolDocstrings.get_user_data
-
-print(get_user_data())
